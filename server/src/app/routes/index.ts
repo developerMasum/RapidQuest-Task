fd47d474569b3133c60d37595c209a6ac4e3aa57
developerMasum <developer.masum2001@gmail.com>
2024-08-23 13:12:04 +0600

    added initial file setup

---
+++ server/src/app/routes/index.ts
@@ -0,0 +1,16 @@
+// import { Router } from 'express';
+
+// // import { ProductsRoutes } from '../modules/products/product.route';
+
+// const router = Router();
+
+// const moduleRoutes = [
+//   // {
+//   //   path: '/products',
+//   //   route: ProductsRoutes,
+//   // },
+// ];
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+// export default router;
